4adf1a3736c23004df443cf3cbdda693053a445e
Paul Parkinson <paul.parkinson@gmail.com>
2023-07-10 18:43:21 -0400

    init

--- sql/huggingfacequery.sql
+++ sql/huggingfacequery.sql
@@ -1,30 +1,53 @@
-declare
-  ctx dbms_mle.context_handle_t;
-  SNIPPET CLOB;
+create or replace mle module huggingface_module
+language javascript as
+
+import "mle-js-fetch";
+import "mle-js-oracledb";
+
+export async function huggingfaceDemo(apiToken) {
+
+    if (apiToken === undefined) {
+        throw Error("must provide an API token");
+    }
+
+    const payload = {
+        inputs: "The answer to the universe is [MASK]."
+    };
+    const modelId = "bert-base-uncased";
+
+    const headers = {
+        "Authorization": `Bearer ${apiToken}`
+    };
+
+    const restAPI = `https://api-inference.huggingface.co/models/${modelId}`;
+
+    const resp = await fetch(
+        restAPI, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(payload),
+            credentials: "include"
+        }
+    );
+
+    const resp_json = await resp.json();
+
+    session.execute(
+        `INSERT INTO HUGGINGFACEJSON (id) VALUES (:resp_json)`,
+        [ JSON.stringify(resp_json) ]
+    );
+}
+/
+
+create or replace procedure huggingfacequery(
+    p_API_token varchar2
+) as mle module huggingface_module
+signature 'huggingfaceDemo(string)';
+/
+
+-- this is how you can test the API call
 begin
-  ctx := dbms_mle.create_context();
-  SNIPPET := q'~
-  (async () => {
-   await import('mle-js-fetch');
-   const oracledb = require("mle-js-oracledb");
-   const payload = "{\"inputs\": \"The answer to the universe is [MASK].\"}" ;
-   const modelId = 'bert-base-uncased';
-   const apiToken = 'hf_[yourhuggingfacetoken]';
-   const headers = { 'Authorization': `Bearer ${apiToken}` };
-   const API_URL = `https://api-inference.huggingface.co/models/${modelId}`;
-   const answer = await fetch(API_URL, {
-                  			method: 'POST',
-                  			headers: headers,
-                  			body: JSON.stringify(payload),
-                  			credentials: 'include'
-                  		}).then(response => response.json());
-   const insertJsonSql = 'INSERT INTO HUGGINGFACEJSON (id) VALUES (:jsonAnswer)';
-   const jsonAnswer = JSON.stringify(answer, undefined, 4);
-   await  oracledb.defaultConnection().execute(insertJsonSql, {jsonAnswer});
-   console.log(JSON.stringify(answer, undefined, 4));
-  })();
-  ~';
-  dbms_mle.eval(ctx, 'JAVASCRIPT', SNIPPET, options =>'js_mode=module');
-  dbms_mle.drop_context(ctx);
+    utl_http.set_wallet('system:');
+    huggingfacequery('[hf_yourhuggingfacetokenhere]');
 end;
 /
